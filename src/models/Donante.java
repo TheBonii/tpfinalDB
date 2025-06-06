c9ad3fc7a9d9b195689148c4548ba5ca753fd335
TheBonii <Lucasbonino5@hotmail.com>
2025-06-06 00:30:45 -0300

    feat: add attributes, getters and setters to Donante model

--- src/models/Donante.java
+++ src/models/Donante.java
@@ -1,4 +1,42 @@
 package models;
 
+import java.math.BigInteger;
+
 public class Donante {
+ private int dni;
+ private String ocupacion;
+ private long cuit;
+
+    public Donante() {
+    }
+
+    public Donante(int dni, String ocupacion, long cuit) {
+        this.dni = dni;
+        this.ocupacion = ocupacion;
+        this.cuit = cuit;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getOcupacion() {
+        return ocupacion;
+    }
+
+    public void setOcupacion(String ocupacion) {
+        this.ocupacion = ocupacion;
+    }
+
+    public long getCuit() {
+        return cuit;
+    }
+
+    public void setCuit(long cuit) {
+        this.cuit = cuit;
+    }
 }
