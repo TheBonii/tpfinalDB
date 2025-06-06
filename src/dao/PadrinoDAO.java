f6593df193ec18bcf95249507b333b8114de1aed
TheBonii <Lucasbonino5@hotmail.com>
2025-06-06 00:19:17 -0300

    new: create basic models and DAOs

---
+++ src/dao/PadrinoDAO.java
@@ -0,0 +1,16 @@
+package dao;
+
+public class PadrinoDAO {
+    public boolean insertar(Padrino padrino) {
+    }
+
+    public List<PadrinoPrograma> listarConProgramas() {
+
+    }
+
+    public Padrino buscarPorDni(int dni) {
+    }
+
+    public List<Padrino> listarTodos() {
+    }
+}
