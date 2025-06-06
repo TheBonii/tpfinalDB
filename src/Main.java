854675a6135a50b751e503f026782821d17a4be0
TheBonii <Lucasbonino5@hotmail.com>
2025-06-06 01:35:40 -0300

    feat: implement padrino insert method and add test in main

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,44 @@
 import dao.DBConnection;
+import dao.PadrinoDAO;
+import models.Padrino;
 
 import java.sql.Connection;
+import java.sql.Date;
 
 public class Main {
     public static void main(String[] args) {
+        Padrino padrino = new Padrino();
+
+
+        padrino.setDni(123456);
+        padrino.setNombre("Juanito");
+        padrino.setApellido("Juanote");
+        padrino.setDireccion("Av. Italia 123");
+        padrino.setCelular("358411123");
+        padrino.setTelefonoFijo("123456890");
+        padrino.setEmail("Juanito.juanote@testeando.com");
+        padrino.setFacebook("juanito.juanote");
+        padrino.setCodigoPostal(5800);
+        padrino.setFechaNacimiento(Date.valueOf("1990-05-15")); // Formato: YYYY-MM-DD
+        PadrinoDAO dao = new PadrinoDAO();
+        boolean resultado = dao.insertar(padrino);
+
+        // 3. VER SI FUNCIONÓ
+        if (resultado) {
+            System.out.println("Padrino Insertado");
+        } else {
+            System.out.println("Error padrino NO Insertado");
+        }
+
         try {
             Connection connection = DBConnection.getConnection();
             System.out.println("Funca");
             connection.close();
         } catch (Exception e) {
             System.out.println("No Funca: " + e.getMessage());
-        }    }
+        }
+
+
+
+    }
 }
--- src/dao/PadrinoDAO.java
+++ src/dao/PadrinoDAO.java
@@ -3,12 +3,64 @@ package dao;
 import models.Padrino;
 import models.PadrinoProgram;
 
-import java.util.List;
 
+import java.util.List;
+import java.sql.*;
 public class PadrinoDAO {
 
     public boolean insertar(Padrino padrino) {
-        return true;
+        Connection connection = null;
+        try{
+            String driver = "com.mysql.cj.jdbc.Driver";
+            String url = "jdbc:mysql://localhost:3306/Proyecto";
+            String username = "root";
+            String password = "root";
+
+            Class.forName(driver);
+            connection = DriverManager.getConnection(url, username, password);
+
+            connection.setAutoCommit(false);
+            String query = "INSERT INTO Padrino (dni, nombre, apellido, direccion, celular, telefono_fijo, email, facebook, codigo_postal, fecha_nacimiento) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+            PreparedStatement statement = connection.prepareStatement(query);
+
+
+            statement.setInt(1, padrino.getDni());
+            statement.setString(2, padrino.getNombre());
+            statement.setString(3, padrino.getApellido());
+            statement.setString(4, padrino.getDireccion());
+            statement.setString(5, padrino.getCelular());
+            statement.setString(6, padrino.getTelefonoFijo());
+            statement.setString(7, padrino.getEmail());
+            statement.setString(8, padrino.getFacebook());
+            statement.setInt(9, padrino.getCodigoPostal());
+            statement.setDate(10, padrino.getFechaNacimiento());
+
+            statement.executeUpdate();
+
+            connection.commit();
+            return true;
+
+
+        } catch (ClassNotFoundException cnfe) {
+            System.err.println("Error loading driver: " + cnfe);
+            return false;
+        } catch (SQLException sqle) {
+            try {
+                System.err.println("Error en BD: " + sqle);
+                connection.rollback();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return false;
+        } finally {
+        try {
+            if (connection != null) connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     }
 
     public List<PadrinoProgram> listarConProgramas() {
